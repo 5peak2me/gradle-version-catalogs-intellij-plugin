59ae2037ed49326b24ea169bb0f27966b613071d
5peak2me <461260911@qq.com>
2025-01-26 13:34:17 +0800

    Fix #4

--- src/main/kotlin/com/faendir/intellij/gradleVersionCatalogs/kotlin/inlayhints/VersionCatalogInlayHintsProvider.kt
+++ src/main/kotlin/com/faendir/intellij/gradleVersionCatalogs/kotlin/inlayhints/VersionCatalogInlayHintsProvider.kt
@@ -33,7 +33,7 @@ class VersionCatalogInlayHintsProvider : InlayHintsProvider<NoSettings> {
         if (file is KtFile && file.name == GradleConstants.KOTLIN_DSL_SCRIPT_NAME) {
             return object : FactoryInlayHintsCollector(editor) {
                 override fun collect(element: PsiElement, editor: Editor, sink: InlayHintsSink): Boolean {
-                    if (element.elementType is KtDotQualifiedExpressionElementType) {
+                    if (element.elementType is KtDotQualifiedExpressionElementType && element.nextSibling == null) {
                         val accessor = BuildGradleKtsPsiCache.findAccessor(element)
                         if (accessor != null && BuildGradleKtsPsiCache.findAccessor(element.parent) == null) {
                             val referencedElement = element.project.findInVersionsTomlKeyValues({ VersionsTomlPsiCache.getDefinitions(it, accessor.type) }, accessor.id)
