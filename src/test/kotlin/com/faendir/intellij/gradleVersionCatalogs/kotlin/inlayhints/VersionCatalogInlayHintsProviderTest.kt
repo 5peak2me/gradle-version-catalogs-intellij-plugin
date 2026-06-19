154fc497edd6a1b2ea0fe33e4e16008b484490cf
5peak2me <461260911@qq.com>
2026-06-19 16:19:05 +0800

    Add Kotlin inlay hints provider test

--- src/test/kotlin/com/faendir/intellij/gradleVersionCatalogs/kotlin/inlayhints/VersionCatalogInlayHintsProviderTest.kt
+++ src/test/kotlin/com/faendir/intellij/gradleVersionCatalogs/kotlin/inlayhints/VersionCatalogInlayHintsProviderTest.kt
@@ -10,19 +10,72 @@ import com.intellij.testFramework.utils.inlays.InlayHintsProviderTestCase
  * https://github.com/JetBrains/intellij-community/blob/master/plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/codeInsight/hints/AbstractKotlinReferenceTypeHintsProviderTest.kt
  * https://plugins.jetbrains.com/docs/intellij/inlay-hints.html#advanced-inlay-hints
  */
-class VersionCatalogInlayHintsProviderTest: InlayHintsProviderTestCase() {
+class VersionCatalogInlayHintsProviderTest : InlayHintsProviderTestCase() {
 
+    fun testLibraryAccessorHint() {
+        addGradleApiStubs()
+        myFixture.addFileToProject(
+            "src/main/java/Libs.java",
+            """
+            import org.gradle.api.artifacts.MinimalExternalModuleDependency;
+            import org.gradle.api.provider.Provider;
 
-    override fun setUp() {
-        super.setUp()
+            public class Libs {
+                public Provider<MinimalExternalModuleDependency> getJunit() {
+                    return null;
+                }
+            }
+            """.trimIndent()
+        )
+        myFixture.addFileToProject(
+            "gradle/libs.versions.toml",
+            """
+            [libraries]
+            junit = "junit:junit:4.13.2"
+            """.trimIndent()
+        )
 
+        val source = """
+            import org.gradle.api.artifacts.MinimalExternalModuleDependency
+            import org.gradle.api.provider.Provider
+
+            val libs = Libs()
+
+            dependencies {
+                implementation(libs.junit)
+            }
+
+            fun dependencies(block: Dependencies.() -> Unit) {}
+
+            class Dependencies {
+                fun implementation(dependency: Any?) {}
+            }
+            """.trimIndent()
+
+        myFixture.configureByText("build.gradle.kts", source)
+
+        val textWithInlays = dumpInlayHints(source, VersionCatalogInlayHintsProvider())
+
+        assertTrue(textWithInlays, textWithInlays.contains("junit:junit:4.13.2"))
+        assertEquals(1, Regex("junit:junit:4\\.13\\.2").findAll(textWithInlays).count())
     }
 
-    fun `test`() {
+    private fun addGradleApiStubs() {
+        myFixture.addFileToProject(
+            "src/main/kotlin/org/gradle/api/provider/Provider.kt",
+            """
+            package org.gradle.api.provider
 
+            interface Provider<T>
+            """.trimIndent()
+        )
+        myFixture.addFileToProject(
+            "src/main/kotlin/org/gradle/api/artifacts/MinimalExternalModuleDependency.kt",
+            """
+            package org.gradle.api.artifacts
 
-//        doTestProvider("VersionCatalogInlayHintsProvider")
-
+            interface MinimalExternalModuleDependency
+            """.trimIndent()
+        )
     }
-
 }
